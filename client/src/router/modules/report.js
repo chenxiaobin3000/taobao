385f11df507c10cbbf2530ac3abe2bcc7bb22bf9
chenxiaobin3000 <chenxiaobin3000@gmail.com>
2026-01-26 02:31:14 +0800

    客户端报告框架

---
+++ client/src/router/modules/report.js
@@ -0,0 +1,45 @@
+import Layout from '@/layout'
+
+export default {
+  path: '/report',
+  component: Layout,
+  name: 'report',
+  meta: {
+    title: '报告管理',
+    roles: [5000]
+  },
+  children: [{
+    path: 'yearReport', component: () => import('@/views/report/yearReport'),
+    name: 'middle_yearReport', meta: { title: '年报汇总', roles: [5001] }
+  }, {
+    path: 'dayReport', component: () => import('@/views/report/dayReport'),
+    name: 'middle_dayReport', meta: { title: '日报汇总', roles: [5002] }
+  }, {
+    path: 'goodReport', component: () => import('@/views/report/goodReport'),
+    name: 'middle_goodReport', meta: { title: '商品汇总', roles: [5003] }
+  }, {
+    path: 'goodRadar', component: () => import('@/views/report/goodRadar'),
+    name: 'middle_goodRadar', meta: { title: '商品雷达', roles: [5004] }
+  }, {
+    path: 'costReport', component: () => import('@/views/report/costReport'),
+    name: 'middle_costReport', meta: { title: '成本汇总', roles: [5005] }
+  }, {
+    path: 'fakeReport', component: () => import('@/views/report/fakeReport'),
+    name: 'middle_fakeReport', meta: { title: '刷单汇总', roles: [5006] }
+  }, {
+    path: 'successReport', component: () => import('@/views/report/successReport'),
+    name: 'middle_successReport', meta: { title: '订单完成', roles: [5007] }
+  }, {
+    path: 'closeReport', component: () => import('@/views/report/closeReport'),
+    name: 'middle_closeReport', meta: { title: '订单关闭', roles: [5008] }
+  }, {
+    path: 'shippedReport', component: () => import('@/views/report/shippedReport'),
+    name: 'middle_shippedReport', meta: { title: '已发货', roles: [5009] }
+  }, {
+    path: 'paidReport', component: () => import('@/views/report/paidReport'),
+    name: 'middle_paidReport', meta: { title: '未发货', roles: [5010] }
+  }, {
+    path: 'unpaidReport', component: () => import('@/views/report/unpaidReport'),
+    name: 'middle_unpaidReport', meta: { title: '未付款', roles: [5011] }
+  }]
+}
