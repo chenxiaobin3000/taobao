0a9ac0bb5e219e831f9778c021f625084307d0ef
chenxiaobin3000 <chenxiaobin3000@gmail.com>
2026-03-21 16:24:47 +0800

    解决刷新丢失数据问题

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,30 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  onBeforeUnmount() {
+    // 必须手动注销监听
+    window.removeEventListener('beforeunload', this.beforeunloadHandler)
+  },
+  created() {
+    // 监听刷新事件
+    window.addEventListener('beforeunload', this.beforeunloadHandler)
+
+    // 初始化页面时读取数据
+    const store = JSON.parse(sessionStorage.getItem('store'))
+    if (store) {
+      this.$store.commit('header/SET_HEADER_SHOP', store.shop)
+    }
+  },
+  methods: {
+    beforeunloadHandler() {
+      // 页面刷新之前保存数据
+      if (this.$store.getters.shop > 0) {
+        sessionStorage.setItem('store', JSON.stringify({
+          shop: this.$store.getters.shop
+        }))
+      }
+    }
+  }
 }
 </script>
