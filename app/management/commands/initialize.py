60fc3bd4f6bece39ad1d39477d3ba19b75f17f5d
chenxiaobin3000 <chenxiaobin3000@gmail.com>
2026-03-27 01:25:42 +0800

    修复bug

--- app/management/commands/initialize.py
+++ app/management/commands/initialize.py
@@ -96,7 +96,7 @@ class Command(BaseCommand):
         UserShop.objects.add(3, 5)
 
         User.objects.add('小月', '13000000001', 1, 1)
-        Account.objects.add('chenxb', '3c278fb4abd387f394292f67ea7d3ad8', 2)
+        Account.objects.add('chenxb', '3c278fb4abd387f394292f67ea7d3ad8', 1)
         User.objects.add('小天使', '13000000002', 1, 2)
-        Account.objects.add('huangx', '21218cca77804d2ba1922c33e0151105', 3)
+        Account.objects.add('huangx', '21218cca77804d2ba1922c33e0151105', 2)
         print("init success")
