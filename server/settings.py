f60f3847d4adeeaacc83dfd91bc52d4dbd943085
chenxiaobin3000 <chenxiaobin3000@gmail.com>
2026-04-26 18:11:53 +0800

    移除默认后台

--- server/settings.py
+++ server/settings.py
@@ -30,23 +30,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'corsheaders', # pip install django-cors-headers
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -81,20 +71,6 @@ DATABASES = {
         },
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [{
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    }, {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    }, {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    }, {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
